7ac8a72a97159a44dd3b11e3554ec9614f59f9e5
Ahmad Taiwo <ahmadtaiwo15@gmail.com>
2025-03-30 23:31:42 -1100

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+let btnRef = document.querySelectorAll('.button-option')
+let popupRef = document.querySelector('.popup')
+let newgameBtn = document.getElementById('new-game')
+let restartBtn = document.getElementById('restart')
+let msgRef = document.getElementById('message')
+
+//Winning Pattern Array
+let winningPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+//Player 'X' plays first
+let xTurn = true
+let count = 0
+
+//disbale all buttons
+const disableButtons = () => {
+    btnRef.forEach((element) => (element.disable = true))
+    //enable popup
+    popupRef.classList.remove('hide')
+}
+
+//enable all buttons (for new game and restart)
+const  enableButtons = () => {
+    btnRef.forEach((element) => {
+        element.innerText = ''
+        element.disabled = false
+    })
+    //disable popup
+    popupRef.classList.add('hide')
+}
+
+//this funtion is executed when a player wins
+const winFunction = (letter) => {
+    disableButtons()
+    if (letter == 'X') {
+        msgRef.innerHTML = `&#x1F389 <br> 'X' Wins`
+    } else {
+        msgRef.innerHTML = `&#x1F389' <br> 'O' Win`
+    } 
+}
+
+//Funtion for draw
+const drawFunction = () => {
+    disableButtons()
+    msgRef.innerHTML = `&#x1F60E <br>  It's a Draw`
+}
+
+//new game
+newgameBtn.addEventListener('click', () => {
+    count = 0
+    enableButtons()
+})
+
+restartBtn.addEventListener('click', () => {
+    count = 0
+    enableButtons()
+})
+
+//win logic 
+const winChecker = () => {
+    //Loop through all win patterns
+    for (let i of winningPattern) {
+        let [element1, element2, element3] = [
+            btnRef[i[0]].innerText,
+            btnRef[i[1]].innerText,
+            btnRef[i[2]].innerText,
+        ]
+        //check if elements are filled
+        //if 3 empty elements are same and would give win as would
+        if (element1 != '' && (element2 != '') & (element3 != '')) {
+            if (element1 == element2 && element2 == element3) {
+                //if all 3 buttons have same value to win  then pass the value to winFunction
+                winFunction(element1)
+            }
+        }
+    }
+}
+
+//display X/O on click
+btnRef.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (xTurn) {
+            xTurn = false
+            //display X
+            element.innerText = 'X'
+            element.disabled = true
+        } else {
+            xTurn = true
+            //display O
+            element.innerText = 'O'
+            element.disabled = true
+        }
+        //increment count on each click
+        count += 1
+        if(count === 9) {
+            drawFunction()
+        }
+        //checker for win on every click
+        winChecker()
+    })   
+})
+
+//enable buttons and disable popup on load page
+window.onload = enableButtons
---
+++ readme.md
@@ -0,0 +1,27 @@
+### Links
+
+- Solution URL: [Solution](https://github.com/Solataiwo-15/Simple-To-do-List-Web-App)
+- Live Site URL: [Live Site](https://simple-to-do-list-web-app-pi.vercel.app/)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Vanilla JavaScript
+
+
+### Useful resources
+
+- [video on HTML](https://youtu.be/916GWv2Qs08?si=JsBMLzR6ECvkX2ic) - This helped me to learn and gain insightful knowledge about HTML and how it works. I really liked this pattern and will use it going forward.
+- [Video on CSS](https://youtu.be/OXGznpKZ_sA?si=6fIG2ENTPmG7glFl) - This is an amazing video tutorial which helped me finally understand how to style my  codes professionally. I'd recommend it to anyone still learning this concept.
+- [Video on JAVASCRIPT](https://www.youtube.com/watch?v=2Ji-clqUYnA) - This is a full and fundamental video that helped me alot in understanding hoq Javascript works and how to write JavaScript efficiently. I'd recommend it to anyone still learning this concept.
+
+
+
+## Author
+
+- Frontend Mentor - [My Profile](https://www.frontendmentor.io/profile/Solataiwo-15)
+- LinkedIn - [My LinkedIn Page](https://www.linkedin.com/in/ahmad-taiwo/)
